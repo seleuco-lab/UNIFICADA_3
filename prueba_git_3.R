aafa28a07a320dca6bafebafb7fa731086a13f6d
selecuco-lab <jose.hoyos@usach.cl>
2023-05-12 16:40:26 -0400

    prueba 4

--- prueba_git_3.R
+++ prueba_git_3.R
@@ -1,3 +1,21 @@
 library(psych)
 library(openxlsx)
 library(dplyr)
+
+MU_2018<-
+  read.xlsx("MU 2018 ALT.xlsx")
+
+MU_2018<-
+  cbind(ANHO_MU=with(MU_2018, ifelse(N_DOC!=0, "2018", "OTRO")), MU_2018)
+
+MU_2019<-
+  read.xlsx("MU MAYO 2019.xlsx")
+
+
+MU_2019<-
+  cbind(ANHO_MU=with(MU_2019, ifelse(N_DOC!=0, "2019", "OTRO")), MU_2019)
+
+MU_2020<-
+  read.xlsx("MU MAYO 2020.xlsx")
+
+
